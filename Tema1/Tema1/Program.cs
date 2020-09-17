58e1f397d6010e36d5a036746d8c8ded9e5d7274
LauraIloiu <iloiu_laura@yahoo.com>
2020-09-17 20:44:35 +0300

    Comment

--- Tema1/Tema1/Program.cs
+++ Tema1/Tema1/Program.cs
@@ -18,7 +18,7 @@ namespace Tema1
             //Exercitiu8();
             //Exercitiu9();
             //Exercitiu10();
-            Exercitiu11();
+            //Exercitiu11();
             Exercitiu12();
             //Exercitiu13();
             Exercitiu14();
